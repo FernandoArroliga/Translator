0fd9bb0a0295e678b2b06aeb5272d7b79a8c4a04
Fernando Arroliga <FernandoArroliga@hotmail.com>
2023-08-14 18:22:24 -0600

    "Creating the basic style of the app"

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# Importing the useful libraries and modules
 from customtkinter import CTk, CTkFrame, CTkButton, CTkLabel, CTkEntry,CTkComboBox, set_default_color_theme, set_appearance_mode
 from tkinter import messagebox, END
 from tkinter import ttk, Text, Label
 import googletrans
 from PIL import Image, ImageTk
 
+# Setting the application theme
 set_default_color_theme("green")
 set_appearance_mode("dark")
 
@@ -18,8 +20,8 @@ class App(CTk):
         self.maxsize(width=900, height=600)    
         
         # frame size
-        frame_width = 470
-        frame_height = 600  
+        frame_width = 500
+        frame_height = 650  
         
         # background image
         self.background_image = Image.open("background.jpg").resize((900,600))
@@ -51,7 +53,7 @@ class MainFrame(CTkFrame):
         self.configure(width=self.width, height=self.height)
         
         # configure grid system
-        self.grid_rowconfigure((0, 1, 2, 3, 4, 5), weight=1)  
+        self.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)  
         self.grid_columnconfigure((0, 1, 2), weight=1)
         
         # --------------variables------------------
@@ -65,6 +67,8 @@ class MainFrame(CTkFrame):
         self.create_widgets()
         
     def create_widgets(self):
+        
+        # first row
         self.original_combo = CTkComboBox(
             self, 
             width=100,
@@ -74,11 +78,8 @@ class MainFrame(CTkFrame):
             text_color="black",
             values=self.language_list)
         self.original_combo.set("")
-        self.original_combo.grid(row=0, column=0, sticky="sw", padx=20)
-        
-        self.original_text = Text(self, height=10, width=40)
-        self.original_text.grid(row=1, column=0, pady=20, padx=20, columnspan=3, sticky="news")
-       
+        self.original_combo.grid(row=0, column=0, sticky="sw", padx=20, pady=20)
+
         self.clear_button = CTkButton(
             self, 
             text="Clear", 
@@ -87,11 +88,27 @@ class MainFrame(CTkFrame):
             hover_color="#dcffd7",
             text_color="black",            
             command=self.master.clear)
-        self.clear_button.grid(row=0, column=2, sticky="sw")
-       
+        self.clear_button.grid(row=0, column=2, sticky="sw", padx=20, pady=20)
+        
+        self.translate_button = CTkButton(
+            self, 
+            text="Translate", 
+            font=("Helvetica", 20), 
+            fg_color="#99ffff",
+            hover_color="#dcffd7",
+            text_color="black",
+            command=self.master.translate_it)
+        self.translate_button.grid(row=0, column=1,sticky="sw", padx=20, pady=20)            
+        
+        # Second row
+        self.original_text = Text(self, height=10, width=40)
+        self.original_text.grid(row=1, column=0, pady=20, padx=20, columnspan=3, sticky="news")
+    
+        # Third row   
         self.translated_text = Text(self, height=10, width=40)
         self.translated_text.grid(row=2, column=0, pady=10, padx=20, columnspan=3, sticky="news")
-        
+
+        # fourth row
         self.translated_combo = CTkComboBox(
             self, 
             width=100,
@@ -102,17 +119,6 @@ class MainFrame(CTkFrame):
             values=self.language_list)
         self.translated_combo.set("")
         self.translated_combo.grid(row=3, column=0, padx=20, sticky="nw")
-        
-        self.translate_button = CTkButton(
-            self, 
-            text="Translate", 
-            font=("Helvetica", 20), 
-            fg_color="#99ffff",
-            hover_color="#dcffd7",
-            text_color="black",
-            command=self.master.translate_it)
-        self.translate_button.grid(row=0, column=1,sticky="sw")
-        
         
 # Running the application        
 if __name__ == "__main__":
